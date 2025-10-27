2b118916c33ee3d27e7a9f868050baaa9a5e76aa
Marcos Queiroz <marcosamfq123@gmail.com>
2025-10-27 16:40:20 -0300

    chore: initial commit

---
+++ src/main/java/com/sigesi/sigesi/SigesiApplication.java
@@ -0,0 +1,13 @@
+package com.sigesi.sigesi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigesiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SigesiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/sigesi/SIGESI/SigesiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sigesi.SIGESI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigesiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
