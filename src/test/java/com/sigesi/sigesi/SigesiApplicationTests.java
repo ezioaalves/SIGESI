0367db54c4bde314453e6f98221f98b916045832
Marcos Queiroz <marcosamfq123@gmail.com>
2025-10-27 16:49:06 -0300

    fix: changing package name for tests

--- src/test/java/com/sigesi/sigesi/SigesiApplicationTests.java
+++ src/test/java/com/sigesi/sigesi/SigesiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.sigesi.SIGESI;
+package com.sigesi.sigesi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SigesiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
