e6d9b9289e997780adc569d064cfbae4c7f99778
Joel Júnior <joel.fmjr@gmail.com>
2025-10-28 13:39:25 -0300

    feat: Ajusta nomes das entidades no Swagger UI
    Remove -controller do nome das entidades exibidas no Swagger UI
    para melhorar a clareza e consistência.

--- src/main/java/com/sigesi/sigesi/jazigos/JazigoController.java
+++ src/main/java/com/sigesi/sigesi/jazigos/JazigoController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 /**
@@ -21,6 +22,7 @@ import jakarta.validation.Valid;
  */
 @RestController
 @RequestMapping("/api/jazigos")
+@Tag(name = "jazigo")
 public class JazigoController {
 
   @Autowired
