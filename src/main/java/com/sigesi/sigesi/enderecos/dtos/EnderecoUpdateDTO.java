950163b8324520ee26a0f466f7d25f03cfd91fe4
Marcos Queiroz <marcosamfq123@gmail.com>
2025-11-05 15:15:13 -0300

    fix: adiciona validação not blank no dto update de endereco

--- src/main/java/com/sigesi/sigesi/enderecos/dtos/EnderecoUpdateDTO.java
+++ src/main/java/com/sigesi/sigesi/enderecos/dtos/EnderecoUpdateDTO.java
@@ -1,5 +1,6 @@
 package com.sigesi.sigesi.enderecos.dtos;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +9,15 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class EnderecoUpdateDTO {
+  @NotBlank(message = "Logradouro não pode ser vazio")
   private String logradouro;
+
+  @NotBlank(message = "Numero não pode ser vazio")
   private String numero;
+
+  @NotBlank(message = "bairro não pode ser vazio")
   private String bairro;
+
+  @NotBlank(message = "Referencia não pode ser vazio")
   private String referencia;
 }
