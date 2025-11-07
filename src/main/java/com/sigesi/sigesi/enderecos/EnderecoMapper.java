3c94563c4b7448ff8ff6265cd4cd6fdef19d9f81
Joel Júnior <87378792+joel-fmjr@users.noreply.github.com>
2025-11-07 13:18:48 -0300

    fix: adiciona @Mapping para ignorar campo id no EnderecoMapper (#14)
    Corrige erro "Unmapped target property: id" do MapStruct adicionando
    anotações @Mapping(target = "id", ignore = true) nos métodos toEntity()
    e updateFromDto(). O campo id é gerado automaticamente pelo banco de
    dados e não deve ser mapeado a partir dos DTOs.

--- src/main/java/com/sigesi/sigesi/enderecos/EnderecoMapper.java
+++ src/main/java/com/sigesi/sigesi/enderecos/EnderecoMapper.java
@@ -3,6 +3,7 @@ package com.sigesi.sigesi.enderecos;
 import java.util.List;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
@@ -13,11 +14,13 @@ import com.sigesi.sigesi.enderecos.dtos.EnderecoUpdateDTO;
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface EnderecoMapper {
 
+  @Mapping(target = "id", ignore = true)
   Endereco toEntity(EnderecoCreateDTO dto);
 
   EnderecoResponseDTO toDto(Endereco entity);
 
   List<EnderecoResponseDTO> toDtoList(List<Endereco> entities);
 
+  @Mapping(target = "id", ignore = true)
   void updateFromDto(EnderecoUpdateDTO dto, @MappingTarget Endereco endereco);
 }
