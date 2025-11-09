ab9fff994cb001244677f4cfd9aaadb45d7ffde0
Joel Júnior <joel.fmjr@gmail.com>
2025-11-09 18:06:45 -0300

    feat: Add env config

--- src/main/java/com/sigesi/sigesi/SigesiApplication.java
+++ src/main/java/com/sigesi/sigesi/SigesiApplication.java
@@ -1,5 +1,6 @@
 package com.sigesi.sigesi;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class SigesiApplication {
 
   public static void main(String[] args) {
+    Dotenv dotenv = Dotenv.configure()
+        .ignoreIfMissing()
+        .load();
+
+    dotenv.entries().forEach(entry ->
+        System.setProperty(entry.getKey(), entry.getValue())
+    );
+
     SpringApplication.run(SigesiApplication.class, args);
   }
 
