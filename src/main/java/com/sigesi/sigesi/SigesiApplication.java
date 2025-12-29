ecd5bede528a38a23ce483a500cca66ecd0bd773
Joel Júnior <joel.fmjr@gmail.com>
2025-12-29 11:19:59 -0300

    refactor: Update application properties and compose configuration

--- src/main/java/com/sigesi/sigesi/SigesiApplication.java
+++ src/main/java/com/sigesi/sigesi/SigesiApplication.java
@@ -1,33 +1,13 @@
 package com.sigesi.sigesi;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class SigesiApplication {
 
-  public static void main(String[] args) {
-    Dotenv dotenv = Dotenv.configure()
-        .ignoreIfMissing()
-        .load();
+	public static void main(String[] args) {
+		SpringApplication.run(SigesiApplication.class, args);
+	}
 
-    String[] allowedKeys = {"GOOGLE_CLIENT_ID", "GOOGLE_CLIENT_SECRET"};
-    for (String key : allowedKeys) {
-      String value = dotenv.get(key);
-      if (value != null) {
-        System.setProperty(key, value);
-      }
-    }
-
-    SpringApplication.run(SigesiApplication.class, args);
-  }
-
-  @GetMapping("/")
-  public String home() {
-    return "Bem-vindo à API Spring Boot!";
-  }
 }
