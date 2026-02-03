6e24dfdabbfe151e85b553bf482b62fc7d971e28
Marcos Queiroz <marcosamfq123@gmail.com>
2026-02-03 20:04:50 -0300

    feat: adiciona frontend ao deploy

--- src/main/java/com/sigesi/sigesi/config/SpringConfig.java
+++ src/main/java/com/sigesi/sigesi/config/SpringConfig.java
@@ -1,6 +1,7 @@
 package com.sigesi.sigesi.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,6 +17,9 @@ public class SpringConfig {
 
   @Autowired
   private CustomOidcUserService customOidcUserService;
+
+  @Value("${app.oauth2.failure-redirect}")
+  private String failureRedirect;
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -35,8 +39,7 @@ public class SpringConfig {
             .userInfoEndpoint(userInfo -> userInfo.oidcUserService(customOidcUserService))
             .successHandler(successHandler)
             .failureHandler((request, response, exception) -> {
-              response.sendRedirect(
-                  "http://localhost:3000/?authError=usuario_inativo");
+              response.sendRedirect(failureRedirect);
             }));
 
     return http.build();
